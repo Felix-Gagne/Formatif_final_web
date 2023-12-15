d74ee2581ad1fa6bdcdac2af0cfd24a7ee81e8f2
Félix Gagné <2070094@cegepmontpetit.ca>
2023-12-15 15:12:42 -0500

    Début SignalR

--- examen2.formatif-main/Question 2/Final_5W5_A23/Final_5W5_A23/Hubs/FavoriteColorHub.cs
+++ examen2.formatif-main/Question 2/Final_5W5_A23/Final_5W5_A23/Hubs/FavoriteColorHub.cs
@@ -24,8 +24,10 @@ namespace FavoriteColor.Hubs
             int nbFavorites = _favoriteColorManager.GetNbFavorites(ColorChoice.NO_COLOR);
 
             // TODO: Mettre le client à jour avec la quantité de favoris pour TOUTES les couleurs sur le client qui vient de se connecter avec l'event InitFavorites
+            await Clients.Caller.SendAsync("InitFavorites", _favoriteColorManager.NbFavoritesPerColor);
 
             // TODO: Utiliser l'event UpdateFavorites pour mettre à jour la quantité pour NO_COLOR sur les clients
+            await Clients.All.SendAsync("UpdateFavorites", ColorChoice.NO_COLOR);
         }
 
         // TODO: Quand un utilisateur se déconnecte, il faut appeler _favoriteColorManager.RemoveUser et mettre à jour la quantité pour la couleur que l'utilisateur avait sur les clients
@@ -42,15 +44,25 @@ namespace FavoriteColor.Hubs
             string newGroupName = _favoriteColorManager.GetGroupName(newColor);
 
             // TODO: Utiliser l'event UpdateFavorites pour mettre à jour la quantité pour oldColor sur les clients
+            await Clients.All.SendAsync("UpdateFavorites", oldColor, nbOldColor);
+
             // TODO: Utiliser l'event UpdateFavorites pour mettre à jour la quantité pour newColor sur les clients
+            await Clients.All.SendAsync("UpdateFavorites", newColor);
 
             // TODO: Retirer l'utilisateur de son ancien groupe
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, oldGroupName);
+
             // TODO: Ajouter l'utilisateur à son nouveau groupe
+            await Groups.AddToGroupAsync(Context.ConnectionId, newGroupName);
         }
 
         public async Task SendMessage(string message)
         {
+            ColorChoice color = _favoriteColorManager.GetFavoriteColor(Context.ConnectionId);
+            string groupName = _favoriteColorManager.GetGroupName(color);
+
             // TODO: Envoyer un message seulement aux utilisateurs qui ont choisi la même couleur en utilisant l'évènement ReceiveMsg
+            await Clients.Group(groupName).SendAsync("ReceiveMsg", message);
         }
     }
 }
